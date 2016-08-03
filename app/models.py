2685e677ef3f32a4cd60340c28924867b7395f11
Mike Korcha <mikekorcha@gmail.com>
2016-08-03 09:18:33 -0700

    Add user model

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from .. import db
+
+
+user_roles = db.Table('user_roles',
+	db.Column('user', db.Integer, db.ForeignKey('user.id')),
+	db.Column('role', db.Integer, db.ForeignKey('role.id'))
+)
+
+class User(db.Model):
+	id       = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(32),  unique=True)
+	password = db.Column(db.String(255), unique=True)
+	email    = db.Column(db.String(255), unique=True)
+	name     = db.Column(db.String(255))
+	roles    = db.relationship('Role', secondary=user_roles, backref='users')
+
+class Role(db.Model):
+	id   = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255), unique=True)
