2b3d9475f3b42d295c2ae25d7c69ca852d2445d6
Mike Korcha <mikekorcha@gmail.com>
2016-08-05 19:17:40 -0700

    document role system, add role check helper

--- app/auth.py
+++ app/auth.py
@@ -3,6 +3,11 @@ from models import User
 from flask import current_app, session
 
 
+# Roles are a finite set of a few different roles. The easy way to do this is
+# to just have a bitwise OR operation to grant multiple roles to a user. For
+# example, an admin who was also an instructor could be given the role like
+# `ROLE_ADMIN | ROLE_INSTRUCTOR`. To check, you mask the user role with the 
+# constant like `user.roles & ROLE_ADMIN`
 ROLE_STUDENT    = 0b001
 ROLE_INSTRUCTOR = 0b001
 ROLE_ADMIN      = 0b100
@@ -46,3 +51,9 @@ def pwhash(password):
 	'''
 	salt = bcrypt.gensalt(rounds=current_app.config['BCRYPT_WORK_FACTOR'])
 	return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def has_role(user, role):
+	'''
+	Checks if the user has a given role
+	'''
+	return user.roles & role
