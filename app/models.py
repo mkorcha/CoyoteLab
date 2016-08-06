46acd0ec65cd8698246fdbd1c1c6cf17b98dc2f7
Mike Korcha <mikekorcha@gmail.com>
2016-08-06 12:47:40 -0700

    add course model and relation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
 
 
+user_courses = db.Table('user_courses', 
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
+	db.Column('course_id', db.Integer, db.ForeignKey('course.id'), nullable=False)
+)
+
+
 class User(db.Model):
 	'''
 	Model representing a user
@@ -11,3 +17,18 @@ class User(db.Model):
 	email    = db.Column(db.String(255), unique=True)
 	name     = db.Column(db.String(255))
 	roles    = db.Column(db.Integer)
+
+class Course(db.Model):
+	'''
+	Model representing a course
+	'''
+	id            = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name          = db.Column(db.String(255))
+	webpage       = db.Column(db.String(255))
+	description   = db.Column(db.Text())
+	instructor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	start_date    = db.Column(db.DateTime())
+	end_date      = db.Column(db.DateTime())
+
+	instructor = db.relationship('Instructor', backref=db.backref('courses'))
+	students   = db.relationship('User', secondary=user_courses, backref='courses')
