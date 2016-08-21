66c6c2d07f1eef7d22010047e0ade514afc7c096
Mike Korcha <mikekorcha@gmail.com>
2016-08-21 10:09:30 -0700

    Student can view info on currently enrolled courses and self-drop (instructors must re-add if needed)

--- app/blueprints/student.py
+++ app/blueprints/student.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, redirect, render_template, url_for, flash
+from flask import Blueprint, redirect, render_template, url_for, flash, abort
 from ..auth import authenticated, session_user, ROLE_STUDENT
+from ..models import Course, Enrollment
+from .. import db
 
 
 blueprint = Blueprint('student', __name__, url_prefix='/student')
+
+
+def get_course(course_id):
+	'''
+	Returns a course from the given course ID if the student is enrolled in it,
+	otherwise will abort
+	'''
+	course = Course.query.get(course_id)
+	user = session_user()
+
+	if course == None or not course in user.enrolled or not user.active_in(course):
+		abort(404)
+
+	return course
 
 
 @blueprint.before_request
@@ -21,5 +37,28 @@ def courses():
 	# a reference to the user needs to be kept around, otherwise the 
 	# association proxy for the enrollment table goes stale
 	user = session_user()
-	return render_template('courses/student.jinja', courses=user.enrolled)
+	return render_template('courses/student.jinja', courses=[x for x in user.enrolled if user.active_in(x)])
 
+
+@blueprint.route('/course/<course_id>')
+def course_info(course_id):
+	course = get_course(course_id)
+	return render_template('courses/info.jinja', course=course)
+
+
+@blueprint.route('/course/<course_id>/drop')
+def drop_course(course_id):
+	course = get_course(course_id)
+
+	enrollment = Enrollment.query.filter_by(course=course, user=session_user()).first()
+
+	if enrollment == None:
+		abort(404)
+
+	enrollment.enabled = False
+
+	db.session.commit()
+
+	flash('Course dropped')
+
+	return redirect(url_for('student.courses'))
